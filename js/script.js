4be7bc4856b3963ee5afeeb8ec56df1f16f68c55
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-06-10 10:57:06 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('bot')
+btn.onclick = function () {
+    // console.log('button has been clicked')
+    alert('Welcome')
+}
+
+const btn1 = document.getElementById('but')
+btn1.onclick = function () {
+    //  console.log('button has been clicked')
+    alert('Hello')
+}
+
+const btn2 = document.getElementById('polo')
+btn2.onclick = function () {
+    alert('Belkins has nice products for you!!')
+}
+
+const btn3 = document.getElementById('see')
+btn3.onclick = function () {
+    alert('click here to see more perfumes!!')
+}
+
+const btn4 = document.getElementById('yoo')
+btn4.onclick = function () {
+    alert('We got nice jewelries for you!!')
+}
+
+const btn5 = document.getElementById('loo')
+btn5.onclick = function () {
+    alert('Lets help you make you up!!')
+}
+
